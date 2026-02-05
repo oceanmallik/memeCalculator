eb52e9ecb2cd6e0c88242e5a55b1dd672549b9d4
Ocean Mallik <oceanmallikocean@gmail.com>
2026-02-05 20:47:00 +0600

    Add calculator UI and core logic skeleton
    Add a banner and title plus a full GridContainer of calculator buttons (digits 0-9, ., C, +, -, *, /, =) to Start.tscn and adjust layout properties. Update Start.cs to declare state (operator, first/second numbers), cache the Screen Label node, print a startup message, and add TheCalculation() which computes a result based on theOperator (switch over +, -, *, /). This is a skeleton: button signals, input parsing, result handling/display and error cases (e.g. divide by zero) are not yet wired up.

--- Start.cs
+++ Start.cs
@@ -3,14 +3,44 @@ using System;
 
 public partial class Start : Control
 {
+	private string theOperator = "";
+	private float firstNumber = 0;
+	private float secondNumber = 0;
+
+    private Label screen;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Hello, Godot! ");
+		GD.Print("Engine Has Started! ");
+
+        // Connecting screen node to screen label, lmao :')
+        screen = GetNode<Label>("VBoxContainer/Screen");
+
+
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    private void TheCalculation()
+    {
+        float result = 0;
+        switch (theOperator)
+        {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+                break;
+        }
+    }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
 	}
 }
